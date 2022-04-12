d4494a4e2cec4853e8a3c87b614bb4f565ed751e
Derek Garnett <drkgrntt@gmail.com>
2022-04-12 09:45:24 -0500

    include channels

---
+++ gochannels/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	links := []string{
+		"https://dragonflyer.live",
+		"https://papyrcms.com",
+		"https://derekgarnett.com",
+		"https://haierhomes.com",
+		"https://ramblings.herokuapp.com",
+		"https://heartlandtohome.derekgarnett.com",
+		"https://player.dailyaudiobible.com",
+	}
+
+	c := make(chan string)
+
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+
+	for l := range c {
+		go func(link string) {
+			time.Sleep(time.Second*5)
+			checkLink(link, c)
+		}(l)
+	}
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might be down")
+		c <- link
+		return
+	}
+	fmt.Println(link, "is up!")
+	c <- link
+}
